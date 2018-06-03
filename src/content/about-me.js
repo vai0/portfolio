cbd7b5d21e9cac00c7a4922f4264e288fad7c08e
Justin Chi <j.chi2241@gmail.com>
2018-06-03 02:15:12 -0700

    add pdf

--- src/content/about-me.js
+++ src/content/about-me.js
@@ -6,12 +6,12 @@ export default [
   {
     input: 'Justin.contactInfo',
     return:
-      '["<a href="mailto:j.chi2241@gmail.com">j.chi2241@gmail.com</a>", "<a href="https://www.linkedin.com/in/justin-chi-64b12b37">LinkedIn</a>", "<a href="https://github.com/vai0">github</a>"]',
+      '["<a href="mailto:j.chi2241@gmail.com">j.chi2241@gmail.com</a>", "<a rel="noopener" href="https://www.linkedin.com/in/justin-chi-64b12b37">LinkedIn</a>", "<a rel="noopener" href="https://github.com/vai0">github</a>"]',
   },
   {
     input: 'Justin.resume',
     return:
-      '"<a href="./justinchi_resume.pdf" target="_blank">justinchi.pdf</a>"',
+      '"<a rel="noopener" href="/justinchi_resume.pdf" target="_blank">justinchi.pdf</a>"',
   },
   {
     input: 'Justin.interests',
