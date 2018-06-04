281600a6b83bd172df46f304ea39c83d52bf232e
Justin Chi <j.chi2241@gmail.com>
2018-06-03 17:30:31 -0700

    add analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,5 +14,13 @@ module.exports = {
     `gatsby-plugin-sass`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-94088018-1",
+        anonymize: true,
+        respectDNT: true
+      },
+    },
   ],
 };
